a959eff69e574eda9b89b481d95f5be5f1158bc6
Ian Iversen <ianiversen1@gmail.com>
2023-01-31 12:42:28 -0500

    style: basic landing-page grid layout set

--- landing-page/client/pages/index.tsx
+++ landing-page/client/pages/index.tsx
@@ -5,16 +5,34 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <Image src="/assets/style-palet.png" alt="Paint Bucket" width={35} height={35} />
-      <Image src="/assets/projects.png" alt="Projects" width={35} height={35} />
-      <Image src="/assets/pricing.png" alt="Paint Bucket" width={35} height={35} />
-      <Image src="/assets/fast-websites.png" alt="Paint Bucket" width={35} height={35} />
-      <Image src="/assets/editor.png" alt="Paint Bucket" width={35} height={35} />
-      <Image src="/assets/contact-us.png" alt="Paint Bucket" width={35} height={35} />
-      <Image src="/assets/cms.png" alt="Paint Bucket" width={35} height={35} />
-      <Image src="/assets/blog.png" alt="Paint Bucket" width={35} height={35} />
-      <Image src="/assets/about.png" alt="Paint Bucket" width={35} height={35} />
+    <div className='landing-page'>
+      <div>
+        <Image src="/assets/style-palet.png" alt="Paint Bucket" width={35} height={35} />
+      </div>
+      <div>
+        <Image src="/assets/projects.png" alt="Projects" width={35} height={35} />
+      </div>
+      <div>
+        <Image src="/assets/pricing.png" alt="Pricing" width={35} height={35} />
+      </div>
+      <div>
+        <Image src="/assets/fast-websites.png" alt="fast-websites" width={35} height={35} />
+      </div>
+      <div>
+        <Image src="/assets/editor.png" alt="Editor" width={35} height={35} />
+      </div>
+      <div>
+        <Image src="/assets/contact-us.png" alt="Contact-Us" width={35} height={35} />
+      </div>
+      <div>
+        <Image src="/assets/cms.png" alt="CMS" width={35} height={35} />
+      </div>
+      <div>
+        <Image src="/assets/blog.png" alt="Blog" width={35} height={35} />
+      </div>
+      <div>
+        <Image src="/assets/about.png" alt="About" width={35} height={35} />
+      </div>
     </div>
   )
 }
