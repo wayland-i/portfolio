04a9f32bbb8da79b8002ad9da9bf9c0e73bee7b0
Ian Iversen <ianiversen1@gmail.com>
2023-01-31 13:24:24 -0500

    style: grid display layout essentially done, tbd font, naming, linking

--- landing-page/client/pages/index.tsx
+++ landing-page/client/pages/index.tsx
@@ -8,30 +8,55 @@ const Home: NextPage = () => {
     <div className='landing-page'>
       <div>
         <Image src="/assets/style-palet.png" alt="Paint Bucket" width={35} height={35} />
+        <p>Visuals</p>
       </div>
       <div>
         <Image src="/assets/projects.png" alt="Projects" width={35} height={35} />
+        <p>
+        My Projects
+        </p>
       </div>
       <div>
         <Image src="/assets/pricing.png" alt="Pricing" width={35} height={35} />
+        <p>
+        My Performance
+        </p>
       </div>
       <div>
         <Image src="/assets/fast-websites.png" alt="fast-websites" width={35} height={35} />
+        <p>
+          Contact Me
+        </p>
       </div>
       <div>
         <Image src="/assets/editor.png" alt="Editor" width={35} height={35} />
+        <p>
+          Coming Soon
+        </p>
       </div>
       <div>
         <Image src="/assets/contact-us.png" alt="Contact-Us" width={35} height={35} />
+        <p>
+          Coming Soon
+        </p>
       </div>
       <div>
         <Image src="/assets/cms.png" alt="CMS" width={35} height={35} />
+        <p>
+          Coming Soon
+        </p>
       </div>
       <div>
         <Image src="/assets/blog.png" alt="Blog" width={35} height={35} />
+        <p>
+          My Blog
+        </p>
       </div>
       <div>
         <Image src="/assets/about.png" alt="About" width={35} height={35} />
+        <p>
+        About Me
+        </p>
       </div>
     </div>
   )
