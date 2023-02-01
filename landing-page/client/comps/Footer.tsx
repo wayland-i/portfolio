2579de31813908b6535cda1faef058ae48f39744
Ian Iversen <ianiversen1@gmail.com>
2023-01-31 23:27:19 -0500

    feat: clock added to right side of footer

--- landing-page/client/comps/Footer.tsx
+++ landing-page/client/comps/Footer.tsx
@@ -2,12 +2,21 @@ import React from 'react'
 import Image from 'next/image';
 
 const Footer = () => {
+
+  const date = new Date();
+  const time = date.toLocaleTimeString();
+  console.log(time);
+
   return (
     <div className='footer'>
       <button className='start-button' >
         <Image src="/assets/windows-logo.png" alt="Windows Logo" width={25} height={25} />
         Start
       </button>
+      <div className='footer-right'>
+        <Image src="/assets/utopia_smiley.png" alt="Smiley Face" width={20} height={20}/>
+        <div>{time}</div>
+      </div>
 
     </div>
   )
