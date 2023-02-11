a2d2bb66afc2e1d0a9ed5cda8f84a147c7ec309c
Ian Iversen <ianiversen1@gmail.com>
2023-02-11 17:18:17 -0500

    feat: snake set up half done, issues loading the script into the file after the DOM content has loaded

--- landing-page/client/pages/snake.tsx
+++ landing-page/client/pages/snake.tsx
@@ -1,11 +1,58 @@
 import React from 'react'
+import Head from 'next/head';
+import {game} from '../scripts/snakeLogic';
+
 
 const Snake = () => {
+
+    // let grid = document.querySelector(".grid");
+    // console.log(document.getElementsByClassName('grid')[0])
+    
+    
+    // console.log(document.querySelector('.grid'))
+
+    // document.addEventListener('DOMContentLoaded', () => {
+    //     console.log(document.querySelector('.grid'))
+    // });
+
+
+
+
   return (
     <div>
+
+        <Head>
+          <title>{`Ian Iversen's Portfolio`}</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+          {/* <script src='/file.js' defer></script> */}
+        </Head>
         <h1>Snake</h1>
+
+        {/* {game()} */}
+        {/* { document ? document.onload = function() {
+            game()
+        } : null} */}
+
+        {/* blog HTML */}
+        <h1>Nokia 3310 snake</h1>
+        <div className="scoreDisplay"></div>
+        <div className="grid"></div>
+        <div className="button">
+        <button className="top">top</button>
+        <button className="bottom">bottom</button>
+        <button className="left">left</button>
+        <button className="right">right</button>
+        </div>
+        <div className="popup">
+        <button className="playAgain">play Again</button>
+        </div>
+
+        
+
     </div>
   )
 }
 
 export default Snake;
+
+
---
+++ landing-page/client/scripts/snakeLogic.js
@@ -0,0 +1,35 @@
+
+
+// function onDocumentReady() {
+//     // Access the HTML elements as needed and manipulate them
+//     const grid = document.querySelector('.grid');
+//     grid.style.backgroundColor = 'blue';
+//     console.log('hello')
+//   }
+  
+//   document.addEventListener('DOMContentLoaded', onDocumentReady);
+
+
+export const game = () => {
+    // console.log('hello')
+
+    let grid = document.querySelector(".grid");
+    let popup = document.querySelector(".popup");
+    let playAgain = document.querySelector(".playAgain");
+    let scoreDisplay = document.querySelector(".scoreDisplay");
+    let left = document.querySelector(".left");
+    let bottom = document.querySelector(".bottom");
+    let right = document.querySelector(".right");
+    let up = document.querySelector(".top");
+
+    console.log(grid)
+    console.log(popup)
+    console.log(playAgain)
+    console.log(scoreDisplay)
+    console.log(left)
+    console.log(bottom)
+    console.log(right)
+    console.log(up)
+
+
+}
