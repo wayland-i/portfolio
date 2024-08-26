2a16ee6bae264c93d65dc2fc6b22aeb311ad360e
Ian Iversen <ianiversen1@gmail.com>
2024-08-25 23:19:44 -0400

    style: pictures added to itcsny pictures page

--- landing-page/client/pages/pictures.tsx
+++ landing-page/client/pages/pictures.tsx
@@ -7,7 +7,21 @@ import Head from 'next/head';
 export const pictures = () => {
   return (
     <div>
-        
+        <Head>
+          <title>{`Ian Iversen's Portfolio`}</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+        </Head>
+        <div className='itcsnyPictures'>
+            <h2>ITCSNY Pictures</h2>
+            <Image src="/images/IMG_5674.jpeg" alt="Contact-Us" width={300} height={400} style={{padding: '2%'}}></Image>
+            <Image src="/images/IMG_8340.jpeg" alt="Contact-Us" width={300} height={400}></Image>
+            <Image src="/images/IMG_7402.jpeg" alt="Contact-Us" width={300} height={400} style={{padding: '2%'}}></Image>
+            <Image src="/images/1705352395361.jpeg" alt="Contact-Us" width={300} height={400}></Image>
+            <Image src="/images/IMG_7147.jpeg" alt="Contact-Us" width={300} height={400} style={{padding: '2%'}}></Image>
+            <Image src="/images/IMG_7012.jpeg" alt="Contact-Us" width={300} height={400}></Image>
+            <Image src="/images/IMG_7317.jpeg" alt="Contact-Us" width={300} height={400} style={{padding: '2%'}}></Image>
+            
+        </div>
 
     </div>
   )
